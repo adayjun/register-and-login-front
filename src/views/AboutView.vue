b7f350e91398de121cb1851801497ba3b7bf63c4
adayjun <2667205144@qq.com>
2022-09-16 11:29:35 +0800

    前端开全局代理

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,35 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-table
+    :data="tableData"
+    border:none
+    style="width: 100%">
+    
+    <el-table-column
+      prop="id"
+      label="id"
+      width="120">
+    </el-table-column>
+
+    <el-table-column
+      prop="loginName"
+      label="用户名"
+      width="120">
+    </el-table-column>
+
+    <el-table-column
+      prop="name"
+      label="网名"
+      width="120">
+    </el-table-column>
+    
+    <el-table-column
+      prop="password"
+      label="密码"
+      width="120">
+    </el-table-column>
+
+  </el-table>
   </div>
 </template>
